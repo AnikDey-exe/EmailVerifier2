154ffb2e1872b61b9f9f6b0b46d2541a06a43b11
AnikDey-exe <67157750+AnikDey-exe@users.noreply.github.com>
2024-01-04 19:38:45 -0500

    Change port to env var

--- index.js
+++ index.js
@@ -10,6 +10,8 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 app.use(cors)
 
+const port = process.env.PORT || 3000;
+
 const { createClient } = require("@usewaypoint/client");
 const client = createClient(process.env.API_KEY_USERNAME, process.env.API_KEY_PASSWORD);
 
@@ -45,6 +47,6 @@ app.post("/sendemail", async (req, res) => {
     }
   })
 
-app.listen(3000, "0.0.0.0", () => {
+app.listen(port, "0.0.0.0", () => {
     console.log(`Server listening on port 5000`);
 });
